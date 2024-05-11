57c54523e8983dbf705cd5ac8bb0f0a74d6fffc1
sitogi <takano162@gmail.com>
2024-05-11 21:47:09 +0900

    Refactor update local cache logic to handle errors and restore previous cache state

--- src/update-local-cache.ts
+++ src/update-local-cache.ts
@@ -5,44 +5,41 @@ import { ErrorResponseBody, Program, serviceIds, TVSchedule } from "./types";
 import { getFormattedDate } from "./utils";
 
 const END_POINT = "https://api.nhk.or.jp/v2/pg/list";
-
 const cache = new Cache();
 
 export default async function Command() {
-  // 現在の日付 ~ １週間分の日付の文字列 YYYY-MM-DD の配列を作成する
+  const tempPrevCache = getPrevCacheAndClear();
+
+  try {
+    await storeWeeklyProgramsCache();
+    await updateCommandMetadata({ subtitle: `Last Update: ${getFormattedDate(new Date(), "YYYY-MM-DD HH:mm")}` });
+  } catch (error) {
+    await showToast({
+      style: Toast.Style.Failure,
+      title: "Failed to fetch data",
+      message: error instanceof Error ? error.message : "An error occurred while fetching data.",
+    });
+
+    // restore prev data
+    serviceIds.forEach((sid) => {
+      cache.set(sid, JSON.stringify(tempPrevCache[sid]));
+    });
+  }
+}
+
+async function storeWeeklyProgramsCache(): Promise<void> {
   const weekDates = Array.from({ length: 7 }, (_, i) => {
     const jstDate = new Date(new Date().getTime() + 9 * 60 * 60 * 1000);
     jstDate.setDate(jstDate.getDate() + i);
     return getFormattedDate(jstDate, "YYYY-MM-DD");
   });
 
-  // prev cache data
-  const prevCache: { [key: string]: Program[] } = {};
-  serviceIds.forEach((sid) => {
-    const data = JSON.parse(cache.get(sid) ?? "[]") as Program[];
-    prevCache[sid] = data;
-    return data;
-  });
-
-  serviceIds.forEach(cache.remove);
-
   for (const date of weekDates) {
     const response = await fetch(`${END_POINT}/${preferences.area}/tv/${date}.json?key=${preferences.apiKey}`);
 
     if (!response.ok) {
       const errorResponse = (await response.json()) as ErrorResponseBody;
-      await showToast({
-        style: Toast.Style.Failure,
-        title: "Failed to fetch data",
-        message: errorResponse.error.message,
-      });
-
-      // restore prev data
-      serviceIds.forEach((sid) => {
-        cache.set(sid, JSON.stringify(prevCache[sid]));
-      });
-
-      return;
+      throw new Error(errorResponse.error.message);
     }
 
     const data = (await response.json()) as TVSchedule;
@@ -52,6 +49,16 @@ export default async function Command() {
       cache.set(sid, JSON.stringify([...existed, ...data.list[sid]]));
     });
   }
+}
 
-  await updateCommandMetadata({ subtitle: `Last Update: ${getFormattedDate(new Date(), "YYYY-MM-DD HH:mm")}` });
+function getPrevCacheAndClear() {
+  // prev cache data
+  const prevCache: { [key: string]: Program[] } = {};
+  serviceIds.forEach((sid) => {
+    const data = JSON.parse(cache.get(sid) ?? "[]") as Program[];
+    prevCache[sid] = data;
+    return data;
+  });
+  serviceIds.forEach(cache.remove);
+  return prevCache;
 }
